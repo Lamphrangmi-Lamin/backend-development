69d94055a2f961edbca80f6c63097511f051e0c4
Lamphrangmi <lamphrangmilamin46169@gmail.com>
2026-08-17 10:36:04 +0530

    feat: implement route handling
    - POST /products
    - GET /products/:id

--- products-api/controllers/product.controller.js
+++ products-api/controllers/product.controller.js
@@ -1,7 +1,58 @@
+const { eq } = require("drizzle-orm");
 const db = require("../src");
 const { productsTable } = require("../src/db/schema");
 
 exports.getAllProducts = async (req, res) => {
   const products = await db.select().from(productsTable);
   return res.json(products);
+};
+
+exports.getProductById = async (req, res) => {
+  const id = req.params.id;
+
+  if (isNaN(id)) return res.status(400).json({ error: "Invalid product id" });
+
+  const [product] = await db
+    .select()
+    .from(productsTable)
+    .where(eq(productsTable.id, id));
+
+  if (!product) {
+    return res
+      .status(404)
+      .json({ error: `Product with id ${id} does not exist!` });
+  }
+
+  return res.json(product);
+};
+
+exports.createProduct = async (req, res) => {
+  const { name, price, stock } = req.body;
+
+  if (!name || name.trim() === "")
+    return res.status(400).json({ error: "name is required" });
+
+  if (!price || isNaN(price))
+    return res.status(400).json({ error: "price is required" });
+
+  if (stock === undefined || !Number.isInteger(stock) || stock < 0)
+    return res
+      .status(400)
+      .json({ error: "stock is required and must be a positive integer" });
+
+  if (price <= 0)
+    return res.status(400).json({ error: "price cannot be 0 or negative" });
+
+  const [newProduct] = await db
+    .insert(productsTable)
+    .values({
+      name: name.trim(),
+      price: Number(price),
+      stock,
+    })
+    .returning();
+
+  return res
+    .status(201)
+    .json({ message: "Product created successfully", id: newProduct.id });
 };
--- products-api/routes/product.routes.js
+++ products-api/routes/product.routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const db = require("../src");
 const { productsTable } = require("../src/db/schema");
-const { getAllProducts } = require("../controllers/product.controller");
+const {
+  getAllProducts,
+  getProductById,
+  createProduct,
+} = require("../controllers/product.controller");
 const router = express.Router();
 
 router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
 
 module.exports = router;
